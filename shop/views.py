110a066c10bdaadff0acade4c0028a40f6fb5ca7
Tolikg63 <ttserunyan@inbox.ru>
2024-03-23 14:00:37 +0400

    Starting view

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpRequest
 
-def index(request):
+def index(request: HttpRequest):
     return render(request, 'shop/index.html')
+
+
+def services(request: HttpRequest):
+    pass
